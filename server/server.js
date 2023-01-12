f2e995bea635616f630449069d0d1acd000a05cd
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2023-01-11 16:34:41 -0800

    build

--- server/server.js
+++ server/server.js
@@ -1,45 +1,44 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
 
-const cors=require("cors");
+const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 8443
+const PORT = process.env.PORT || 443;
 
 const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'pass',
+  secret: "pass",
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(require("./controllers/"));
 
 (async () => {
   await sequelize.sync();
-  console.log('Synced!');
+  console.log("Synced!");
 })();
 
 app.listen(PORT, () => {

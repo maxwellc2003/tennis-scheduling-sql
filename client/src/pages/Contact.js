1a93269e1aee6751ac5e7309af95bb4ec66e3028
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2023-01-14 14:46:28 -0800

    build

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,11 +1,28 @@
 import React from "react";
 
 import "../assets/css/contact.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Contact = () => {
   return (
     <main>
-
+      <form class="contact-form">
+        <h2>CONTACT US</h2>
+        <p type="Name:">
+          <input placeholder="Write your name here.."></input>
+        </p>
+        <p type="Email:">
+          <input placeholder="Let us know how to contact you back.."></input>
+        </p>
+        <p type="Message:">
+          <input placeholder="What would you like to tell us.."></input>
+        </p>
+        <button>Send Message</button>
+        <div>
+        <FontAwesomeIcon icon={faEnvelope} />CustomerService@tennisscheduling.com
+        </div>
+      </form>
     </main>
   );
 };

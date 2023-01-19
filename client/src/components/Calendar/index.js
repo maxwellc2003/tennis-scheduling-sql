99047ec4b6f98122a17d620cddd97df8e077e511
maxwellc2003 <107020453+maxwellc2003@users.noreply.github.com>
2023-01-18 20:59:21 -0800

    pop up session info, rendering events all through react using map

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,24 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import moment from "moment";
 
-// We can add more character to this
-
-import initCalendar from "../../assets/js/calendar.js";
 import weekDays from "../../utils/weekDays";
 
 const CalendarEvents = (events) => {
+  // calendar date usage
   const [currentDate, setCurrentDate] = useState(moment());
   const [calendarDays, setCalendarDays] = useState(weekDays(currentDate));
 
-  useEffect(() => {
-    initCalendar(currentDate, events.data);
-
-    setCalendarDays(weekDays(currentDate));
-  }, [currentDate]);
+  // pop up
+  const [hidden, setHidden] = useState(true);
+  const [popUpInfo, setPopUpInfo] = useState({
+    time: "",
+    location: "",
+    min: "",
+    max: "",
+  });
 
   return (
     <div className="calendar">
+      <div className={`pop-up ${hidden ? "hidden" : ""}`}>
+        <h2>{popUpInfo.time}</h2>
+        <h2>Location: {popUpInfo.location}</h2>
+        <h2>Minimum: {popUpInfo.min}</h2>
+        <h2>Maximum: {popUpInfo.max}</h2>
+        <button onClick={() => console.log("rsvp")}>RSVP</button>
+        <a
+          href="#"
+          onClick={() => console.log("view players")}
+          className="view-players"
+        >
+          (View Players)
+        </a>
+
+        <div onClick={() => setHidden(true)} href="#" className="close-pop-up">
+          x
+        </div>
+      </div>
       <div className="calendar-header">
         <div className="calendar-header-left">
           <div className="dropdown">
@@ -56,21 +75,14 @@ const CalendarEvents = (events) => {
           </div>
         </div>
         <div className="calendar-header-date">
-          <i
-            id="arrow-left"
-            className="arrow left"
-            onClick={() =>
-              setCurrentDate(moment(currentDate.subtract(7, "days")))
-            }
-          ></i>
+          <i className="arrow left" onClick={() => changeDateLeft()}></i>
           <h1 id="current-week-header" className="current-week">
-            placeholder
+            {currentDate.startOf("isoweek").format("MMM")}{" "}
+            {currentDate.startOf("isoweek").format("DD")} -{" "}
+            {currentDate.endOf("isoweek").format("MMM")}{" "}
+            {currentDate.endOf("isoweek").format("DD")}
           </h1>
-          <i
-            id="arrow-right"
-            className="arrow right"
-            onClick={() => setCurrentDate(moment(currentDate.add(7, "days")))}
-          ></i>
+          <i className="arrow right" onClick={() => changeDateRight()}></i>
         </div>
 
         <div className="dropdown2">
@@ -90,55 +102,192 @@ const CalendarEvents = (events) => {
       </div>
       <div className="calendar-content">
         <div className="calendar-row">
-          <div id="weekDay1" className="week-day"></div>
-          <div id="monContainer" className="event-container"></div>
+          <div id="weekDay1" className="week-day">
+            <h2>Mon</h2>
+            <h3>{calendarDays[0].slice(3, 5)}</h3>
+          </div>
+          <div id="monContainer" className="event-container">
+            {events.data
+              .filter((data) => data.date === calendarDays[0])
+              .map((event) => (
+                <div
+                  className="card"
+                  key={event.id}
+                  onClick={() => setPopUp(event)}
+                >
+                  <p>{event.event_users.length}</p>
+                  <div className="number-attending-icon"></div>
+                  <h6>{event.time}</h6>
+                </div>
+              ))}
+          </div>
         </div>
         <div className="calendar-row">
-          <div id="weekDay2" className="week-day"></div>
-          <div id="tueContainer" className="event-container"></div>
+          <div id="weekDay2" className="week-day">
+            <h2>Tue</h2>
+            <h3>{calendarDays[1].slice(3, 5)}</h3>
+          </div>
+          <div id="tueContainer" className="event-container">
+            {events.data
+              .filter((data) => data.date === calendarDays[1])
+              .map((event) => (
+                <div
+                  className="card"
+                  key={event.id}
+                  onClick={() => setPopUp(event)}
+                >
+                  <p>{event.event_users.length}</p>
+                  <div className="number-attending-icon"></div>
+                  <h6>{event.time}</h6>
+                </div>
+              ))}
+          </div>
         </div>
         <div className="calendar-row">
-          <div id="weekDay3" className="week-day"></div>
-          <div id="wedContainer" className="event-container"></div>
+          <div id="weekDay3" className="week-day">
+            <h2>Wed</h2>
+            <h3>{calendarDays[2].slice(3, 5)}</h3>
+          </div>
+          <div id="wedContainer" className="event-container">
+            {events.data
+              .filter((data) => data.date === calendarDays[2])
+              .map((event) => (
+                <div
+                  className="card"
+                  key={event.id}
+                  onClick={() => setPopUp(event)}
+                >
+                  <p>{event.event_users.length}</p>
+                  <div className="number-attending-icon"></div>
+                  <h6>{event.time}</h6>
+                </div>
+              ))}
+          </div>
         </div>
         <div className="calendar-row">
-          <div id="weekDay4" className="week-day"></div>
-          <div id="thuContainer" className="event-container"></div>
+          <div id="weekDay4" className="week-day">
+            <h2>Thu</h2>
+            <h3>{calendarDays[3].slice(3, 5)}</h3>
+          </div>
+          <div id="thuContainer" className="event-container">
+            {events.data
+              .filter((data) => data.date === calendarDays[3])
+              .map((event) => (
+                <div
+                  className="card"
+                  key={event.id}
+                  onClick={() => setPopUp(event)}
+                >
+                  <p>{event.event_users.length}</p>
+                  <div className="number-attending-icon"></div>
+                  <h6>{event.time}</h6>
+                </div>
+              ))}
+          </div>
         </div>
         <div className="calendar-row">
-          <div id="weekDay5" className="week-day"></div>
-          <div id="friContainer" className="event-container"></div>
+          <div id="weekDay5" className="week-day">
+            <h2>Fri</h2>
+            <h3>{calendarDays[4].slice(3, 5)}</h3>
+          </div>
+          <div id="friContainer" className="event-container">
+            {events.data
+              .filter((data) => data.date === calendarDays[4])
+              .map((event) => (
+                <div
+                  className="card"
+                  key={event.id}
+                  onClick={() => setPopUp(event)}
+                >
+                  <p>{event.event_users.length}</p>
+                  <div className="number-attending-icon"></div>
+                  <h6>{event.time}</h6>
+                </div>
+              ))}
+          </div>
         </div>
         <div className="calendar-row">
-          <div id="weekDay6" className="week-day"></div>
-          <div id="satContainer" className="event-container"></div>
+          <div id="weekDay6" className="week-day">
+            <h2>Sat</h2>
+            <h3>{calendarDays[5].slice(3, 5)}</h3>
+          </div>
+          <div id="satContainer" className="event-container">
+            {events.data
+              .filter((data) => data.date === calendarDays[5])
+              .map((event) => (
+                <div
+                  className="card"
+                  key={event.id}
+                  onClick={() => setPopUp(event)}
+                >
+                  <p>{event.event_users.length}</p>
+                  <div className="number-attending-icon"></div>
+                  <h6>{event.time}</h6>
+                </div>
+              ))}
+          </div>
         </div>
         <div className="calendar-row">
-          <div id="weekDay7" className="week-day"></div>
-          <div id="sunContainer" className="event-container"></div>
+          <div id="weekDay7" className="week-day">
+            <h2>Sun</h2>
+            <h3>{calendarDays[6].slice(3, 5)}</h3>
+          </div>
+          <div id="sunContainer" className="event-container">
+            {events.data
+              .filter((data) => data.date === calendarDays[6])
+              .map((event) => (
+                <div
+                  className="card"
+                  key={event.id}
+                  onClick={() => setPopUp(event)}
+                >
+                  <p>{event.event_users.length}</p>
+                  <div className="number-attending-icon"></div>
+                  <h6>{event.time}</h6>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
       <div className="calendar-footer">
         <div className="calendar-footer-date">
-          <i
-            onClick={() =>
-              setCurrentDate(moment(currentDate.subtract(7, "days")))
-            }
-            id="arrow-left1"
-            className="arrow left"
-          ></i>
+          <i onClick={() => changeDateLeft()} className="arrow left"></i>
           <h1 id="current-week-footer" className="current-week">
-            placeholder
+            {currentDate.startOf("isoweek").format("MMM")}{" "}
+            {currentDate.startOf("isoweek").format("DD")} -{" "}
+            {currentDate.endOf("isoweek").format("MMM")}{" "}
+            {currentDate.endOf("isoweek").format("DD")}
           </h1>
-          <i
-            onClick={() => setCurrentDate(moment(currentDate.add(7, "days")))}
-            id="arrow-right1"
-            className="arrow right"
-          ></i>
+          <i onClick={() => changeDateRight()} className="arrow right"></i>
         </div>
       </div>
     </div>
   );
+
+  function rsvp(event, auth) {}
+
+  function setPopUp(data) {
+    setHidden(false);
+
+    setPopUpInfo({
+      time: data.time,
+      location: data.location,
+      min: data.min,
+      max: data.max,
+    });
+  }
+
+  function changeDateLeft() {
+    setCurrentDate(moment(currentDate.subtract(7, "days")));
+
+    setCalendarDays(weekDays(currentDate));
+  }
+
+  function changeDateRight() {
+    setCurrentDate(moment(currentDate.add(7, "days")));
+
+    setCalendarDays(weekDays(currentDate));
+  }
 };
 
 export default CalendarEvents;
--- server/controllers/api/user-routes.js
+++ server/controllers/api/user-routes.js
@@ -2,12 +2,31 @@ const router = require("express").Router();
 const User = require("../../models/User.");
 
 // get all users
-
 router.get("/", (req, res) => {
   User.findAll({
     attributes: { exclude: ['password'] }
   })
     .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get a user by id
+router.get("/:id", (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+      res.json(dbUserData);
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
